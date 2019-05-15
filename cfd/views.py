0b268f0fad0f159e49a72003efdc9e828052ecc0
Chibuzor <nwalorc@gmail.com>
2019-05-15 23:06:17 +0100

    Added formsets to views

--- cfd/views.py
+++ cfd/views.py
@@ -27,14 +27,20 @@ def template_list(request, template_name='template_list.html'):
 
 @login_required
 def cfd_create(request, template_name='cfd_form.html'):
-    form = f.CFDForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        return redirect('cfd:cfd_list')
+
+    if request.method == "POST":        
+        form = f.CFDForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            return redirect('cfd:cfd_list')
+
+    formset = formset_factory(f.CFDForm,extra=3)
+    form = f.CFDForm()
 
     context = {
         'form': form,
-        'fieldsets' : cfdAdmin.fieldsets,
+        'formset' : formset,
+        'fieldsets' : form.Meta.fieldsets,
         "RETAIL_90_MAIL_RATES_B_LIST": RETAIL_90_MAIL_RATES_B_LIST,
         "RETAIL_90_MAIL_RATES_G_LIST": RETAIL_90_MAIL_RATES_G_LIST
     }    
@@ -43,12 +49,32 @@ def cfd_create(request, template_name='cfd_form.html'):
 
 @login_required
 def cfd_update(request, pk, template_name='cfd_form.html'):
+    if request.method == "POST":
+        form = f.CFDForm(request.POST or None, instance=record)
+        if form.is_valid():
+            form.save()
+            return redirect('cfd:cfd_list')
+
     record = get_object_or_404(cfd, pk=pk)
-    form = f.CFDForm(request.POST or None, instance=record)
-    if form.is_valid():
-        form.save()
-        return redirect('cfd:cfd_list')
-    return render(request, template_name, {'form': form, "RETAIL_90_MAIL_RATES_B_LIST": RETAIL_90_MAIL_RATES_B_LIST, "RETAIL_90_MAIL_RATES_G_LIST":RETAIL_90_MAIL_RATES_G_LIST})
+    record_year1 = get_object_or_404(cfd, pk=pk)
+    record_year2 = get_object_or_404(cfd, pk=pk)
+
+    formset = formset_factory(f.CFDForm,extra=3)()
+    formset[0].set_initial_values(record)
+    formset[1].set_initial_values(record_year1)
+    formset[2].set_initial_values(record_year2)
+
+    form = f.CFDForm()
+    
+    context = {
+        'form': form,
+        'formset' : formset,
+        'fieldsets' : form.Meta.fieldsets,
+        "RETAIL_90_MAIL_RATES_B_LIST": RETAIL_90_MAIL_RATES_B_LIST,
+        "RETAIL_90_MAIL_RATES_G_LIST": RETAIL_90_MAIL_RATES_G_LIST
+    }
+    
+    return render(request, template_name, context)
 
 @login_required
 def cfd_delete(request, pk, template_name='cfd_delete.html'):
