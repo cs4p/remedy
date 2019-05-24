369769f0522ac6f1d2e4572484e22e06f551056b
Chibuzor <nwalorc@gmail.com>
2019-05-24 12:31:24 +0100

    Added hidden tag to field data

--- cfd/templatetags/filters.py
+++ cfd/templatetags/filters.py
@@ -6,11 +6,12 @@ register = template.Library()
 @register.filter
 def get_field_data(form, field):
     field = form.fields[field].get_bound_field(form, field)
-
+    
     return {
         'name' : field.name,
         'label_tag' : field.label_tag(),
         'field' : field,
+        'hidden' : field.is_hidden,
         'help_text' : field.help_text
     }    
 
