5a9f86a9fc7af613eec007ed584e1672cbe01a2b
Chibuzor <nwalorc@gmail.com>
2019-05-31 11:13:31 +0100

    Added more view tests

--- cfd/tests/test_views.py
+++ cfd/tests/test_views.py
@@ -1,11 +1,12 @@
 from datetime import date
 
 from django.urls import reverse
-from django.template import RequestContext
-from django.test import Client, TestCase, RequestFactory
+from django.forms import formset_factory
+from django.test import Client, TestCase
 
-from cfd.forms import CFDSearchForm
 from cfd.models import cfd, client, User
+from cfd.forms import CFDForm, CFDSearchForm
+from cfd.views import RETAIL_90_MAIL_RATES_B_LIST, RETAIL_90_MAIL_RATES_G_LIST
 
 class CFDListViewTest(TestCase):
     def setUp(self):
@@ -112,3 +113,206 @@ class TemplateListViewTest(TestCase):
         context = response.context
 
         self.assertEqual(len(context['object_list']), 1)
+
+    def test_post(self):
+        self.client.login(username='user', password='pass')
+
+        response = self.client.post(self.url, {})
+
+        self.assertEqual(response.status_code, 405)
+
+    
+class CFDCreateViewTest(TestCase):
+    def setUp(self):
+        self.url = reverse('cfd:cfd_new')
+
+        self.client = Client()
+
+        self.user = User.objects.create(username='user', is_superuser=True)
+        self.user.set_password('pass')
+        self.user.save()
+
+        client1 = client.objects.create(CLIENT_NAME='Test Client')
+
+    def test_unauthenticated_get(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_unauthenticated_post(self):
+        response = self.client.post(self.url)
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_get(self):
+        self.client.login(username='user', password='pass')
+
+        response = self.client.get(self.url)
+
+        self.assertTemplateUsed('cfd_form.html')
+        self.assertEqual(response.status_code, 200)        
+
+        context = response.context
+
+        CFDFormset = formset_factory(CFDForm, extra=3)
+        
+        self.assertTrue(context['create'])
+        self.assertEqual(len(context['formset'].forms), 3)
+        self.assertEqual(context['fieldsets'], CFDForm.Meta.fieldsets)
+        self.assertEqual(context['RETAIL_90_MAIL_RATES_B_LIST'], RETAIL_90_MAIL_RATES_B_LIST)
+        self.assertEqual(context['RETAIL_90_MAIL_RATES_G_LIST'], RETAIL_90_MAIL_RATES_G_LIST)
+        
+
+    def test_post(self):
+        self.client.login(username='user', password='pass')
+
+        management_form_data = {
+            'form-TOTAL_FORMS' : 3,
+            'form-INITIAL_FORMS' : 0,
+            'form-MIN_NUM_FORMS' : 0,
+            'form-MAX_NUM_FORMS' : 1000
+        }
+
+        data = {
+            'CLIENT' : client.objects.first().pk,
+            'CONTRACT_TYPE' : cfd.DROP_DOWN_MENU_CONTRACT_TYPE_CHOICES[0][0],
+            'RETAIL_90_MAIL_RATES_B' : 'N',
+            'RETAIL_90_MAIL_RATES_G' : 'N',
+            'RETAIL_90_MAIL_RATES_B_DS' : 10,
+            'RETAIL_90_MAIL_RATES_G_DS' : 10,
+            'M_RATE_BREAKOUT' : 10
+        }
+
+        form_data = {}
+        form_data.update(management_form_data)
+
+        form = CFDForm()
+
+        # Fills out the form with default data
+        for key in form.fields.keys():
+            value = form.fields[key].initial            
+            
+            if value:
+                temp = {
+                    f'form-0-{key}' : value,
+                    f'form-1-{key}' : value,
+                    f'form-2-{key}' : value
+                }
+
+                form_data.update(temp)
+            
+            elif 'Discount' in form.fields[key].label or 'Fee' in form.fields[key].label:
+                temp = {
+                    f'form-0-{key}' : 1,
+                    f'form-1-{key}' : 1,
+                    f'form-2-{key}' : 1
+                }
+
+                form_data.update(temp)
+
+        for key in data.keys():
+            temp = {
+                f'form-0-{key}' : data[key],
+                f'form-1-{key}' : data[key],
+                f'form-2-{key}' : data[key]
+            }
+
+            form_data.update(temp)
+        
+        response = self.client.post(self.url, form_data)      
+
+        # The view redirects to the list view after a 
+        # successful post request  
+        self.assertEqual(response.status_code, 302)
+
+        self.assertEqual(len(cfd.objects.filter(**data)), 3)
+
+        response = self.client.post(self.url, management_form_data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(len(response.context['formset'].errors), 3)
+
+
+class CFDUpdateViewTest(TestCase):
+    def setUp(self):
+        
+
+        self.client = Client()
+
+        self.user = User.objects.create(username='user', is_superuser=True)
+        self.user.set_password('pass')
+        self.user.save()
+
+        client1 = client.objects.create(CLIENT_NAME='Test Client')
+
+        contract1 = cfd.objects.create(CLIENT=client1, START_DATE=date(2019,10,1), END_DATE=date(2019,10,10))
+        cfd.objects.create(CLIENT=client1, START_DATE=date(2019,10,11), END_DATE=date(2019,11,10))
+        cfd.objects.create(CLIENT=client1, START_DATE=date(2019,10,21), END_DATE=date(2019,11,20))
+
+        self.url = reverse('cfd:cfd_edit', args=[contract1.pk])
+
+    
+    def test_unauthenticated_get(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_unauthenticated_post(self):
+        response = self.client.post(self.url)
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_get(self):
+        self.client.login(username='user', password='pass')
+
+        response = self.client.get(self.url)
+
+        self.assertTemplateUsed('cfd_form.html')
+        self.assertEqual(response.status_code, 200)
+
+        context = response.context
+
+        CFDFormset = formset_factory(CFDForm, extra=3)
+                
+        self.assertEqual(len(context['formset'].forms), 3)
+        self.assertEqual(context['fieldsets'], CFDForm.Meta.fieldsets)
+        self.assertEqual(context['RETAIL_90_MAIL_RATES_B_LIST'], RETAIL_90_MAIL_RATES_B_LIST)
+        self.assertEqual(context['RETAIL_90_MAIL_RATES_G_LIST'], RETAIL_90_MAIL_RATES_G_LIST)
+
+        contract = cfd.objects.get(CLIENT__CLIENT_NAME='Test Client', START_DATE=date(2019,10,1))
+
+        contracts = [contract]
+        contracts.extend(contract.get_subsequent_contracts(2))
+
+        for item in zip(context['formset'].forms, contracts):
+            form, contract = item            
+            for field in form.fields.keys():
+                self.assertEqual(form.fields[field].initial, getattr(contract, field))
+
+    
+    def test_post(self):
+        self.client.login(username='user', password='pass')
+
+        management_form_data = {
+            'form-TOTAL_FORMS' : 3,
+            'form-INITIAL_FORMS' : 0,
+            'form-MIN_NUM_FORMS' : 0,
+            'form-MAX_NUM_FORMS' : 1000
+        }
+
+        form_data = {
+            'form-0-start_date' : '2019-10-18',
+            'form-0-end_date' : '2019-10-30',
+            'form-1-start_date' : '2020-10-18',
+            'form-1-end_date' : '2020-10-30',
+            'form-2-start_date' : '2021-10-18',
+            'form-2-end_date' : '2021-10-30',
+        }
+
+        form_data.update(management_form_data)
+
+        response = self.client.post(self.url, form_data)
+
+        # Checks that the confirmation page came up
+        self.assertTemplateUsed('cfd_confirmation.html')
+        
